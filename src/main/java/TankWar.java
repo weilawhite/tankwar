3cb75f3a35cd6d6b9c35545d1c50aaa1eef0bb08
weilawhite <E04dl4ul1>
2020-07-29 17:56:21 +0800

    在家上傳測試

--- src/main/java/TankWar.java
+++ src/main/java/TankWar.java
@@ -3,10 +3,16 @@ import javax.swing.*;
 public class TankWar {
     public static void main(String[] args) {
         JFrame frame=new JFrame();
+        JFrame frame2=new JFrame();
         frame.add(new GameClient(900,500));
-        frame.setTitle("TankGame");
+        frame.setTitle("TankGame Trial");
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame2.add(new GameClient(200,500));
+        frame2.setTitle("Status");
+        frame2.setVisible(true);
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame2.pack();
     }
 }
